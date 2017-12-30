a05b7dd88460289f2712e9ef580681a6515f4862
Tim Hillhouse <thillhouse2@gmail.com>
2017-12-30 21:43:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  delete object[key];
-  return object;
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
 }
