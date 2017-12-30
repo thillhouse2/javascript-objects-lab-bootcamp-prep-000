6eef6b88ef0406ad8d979ed4fa53b63ad7bae3a4
Tim Hillhouse <thillhouse2@gmail.com>
2017-12-30 21:42:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function updateObjectWithKeyAndValue(object, key, value){
   newObj[key] = value;
   return newObj;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
